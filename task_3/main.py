4fcb0b47d453b5b6cc0a5cfa83b116ab3ace4248
Nuradil Serik <serik.nuradil@gmail.com>
2026-04-13 16:51:07 +0500

    fixes in inputs, returns only NaN or digits

--- task_3/main.py
+++ task_3/main.py
@@ -14,17 +14,25 @@ def gcd(x: int, y: int):
     return x
 
 @app.get("/serik_nuradil_gmail_com")
-def calculate_lcm(x:int | None = None, y:int | None = None):
-    if x is None or y is None or x < 0 or y < 0:
-        return "NaN"
+def calculate_lcm(x:str = "", y:str = ""):
     
-    if x == 0 or y == 0:
-        return f"{0}"
+    try:
+        xi = int(x)
+        yi = int(y)
+        
+        if xi <= 0 or yi <= 0:
+            return PlainTextResponse("NaN")
+    except(ValueError, TypeError):
+        return PlainTextResponse("NaN")
     
-    if (y % x == 0) or (x % y == 0):
-        return f"{x}" if x > y else f"{y}"
     
-    return PlainTextResponse(f"{(x*y)//gcd(x,y)}")
+    # if x == 0 or y == 0:
+    #     return f"{0}"
+    
+    if (yi % xi == 0) or (xi % yi == 0):
+        return f"{xi}" if xi > yi else f"{y}"
+    
+    return PlainTextResponse(f"{(xi*yi)//gcd(xi,yi)}")
             
     
     
